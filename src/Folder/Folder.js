562051f2c736d6b7289863afeec92534c5ff85a6
Luther Karper <luther.karper@live.com>
2020-06-30 08:48:08 -0400

    Fix typo

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -24,7 +24,7 @@ const Folder = (props) => {
               return res.json().then(error => {
                 // then throw it
                 throw error
-              })
+              });
             }
           })
         .then(data => {
